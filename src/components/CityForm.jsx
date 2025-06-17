ec2fa4852d5b9b74b5e77372b400f3856e304103
Md Sharique <msharique006@gmail.com>
2025-06-17 07:49:56 +0530

    minor change

--- src/components/CityForm.jsx
+++ src/components/CityForm.jsx
@@ -10,7 +10,7 @@ export default function CityForm({ city, country, setCity, setCountry, onSubmit 
   };
 
   return (
-    <form onSubmit={handleSubmit} className="mb-4">
+    <form onSubmit={handleSubmit} className="mb-3">
       <div className="row g-2 justify-content-center">
         <div className="col-md-4">
           <input
